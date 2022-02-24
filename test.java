04e82a3409856ddc57d679f9a7e1ecfd8a9fd75b
Tmba1984 <thierry.p.mba@gmail.com>
2022-02-24 22:22:57 +0100

    Thierry Commit message

--- test.java
+++ test.java
@@ -1,11 +1,8 @@
 public class test{
 	public static void main(String[] args){
 		int tmp = 0;
-		if(tmp == 1){
-			System.out.println("Test erfolgreich");
-		}else{
-			tmp = 2;
-			System.out.println("Nicht erfolgreich");
-		}
+
+		System.out.println("Test erfolgreich");
+
 	}
 }
